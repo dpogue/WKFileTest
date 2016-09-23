108c65a67405414e3ff97f431573b979c54f0484
Darryl Pogue <darryl@ayogo.com>
2016-09-23 10:21:59 -0700

    Added test case files

--- WKFileTest/ViewController.swift
+++ WKFileTest/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import WebKit
+
+let USE_WKWEBVIEW = true
 
 class ViewController: UIViewController {
+    
+    var oldView: UIWebView?
+    var webView: WKWebView?
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.oldView = UIWebView()
+        self.webView = WKWebView()
+        
+        if (USE_WKWEBVIEW) {
+            self.view = self.webView!
+        } else {
+            self.view = self.oldView!
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let bundle = Bundle.main;
+        let wwwPath = bundle.path(forResource:"index.html", ofType:"", inDirectory:"www");
+        let url = URL.init(fileURLWithPath: wwwPath!);
+        let dir = url.deletingLastPathComponent();
+        
+        let request = URLRequest.init(url: url);
+        
+        self.oldView?.loadRequest(request);
+        
+        _ = self.webView?.loadFileURL(url, allowingReadAccessTo:dir);
     }
 
     override func didReceiveMemoryWarning() {
